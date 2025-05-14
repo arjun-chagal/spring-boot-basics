0d62ac070f8c4f655111e9705743cba805d9b6cd
arjunchagal <116339514+arjunchagal@users.noreply.github.com>
2025-05-14 22:48:48 +0530

    created a project to showcaase dependency injection

--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/CricketCoach.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/CricketCoach.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class CricketCoach implements Coach{
     @Override
    public String getDailyWorkout(){
-        return null;
+        return "practice fast bowling for an hour!!!";
     }
 }
