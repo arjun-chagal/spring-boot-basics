492fd4b7e44e844fe39b60b2732486a955ff1012
arjun-chagal <2021is_arjunchagalamarri_a@nie.ac.in>
2025-05-31 20:59:59 +0530

    added custom component scanning

--- 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,7 +1,7 @@
 package com.luv2code.springcoredemo.rest;
 
 
-import com.luv2code.springcoredemo.common.Coach;
+import com.luv2code.util.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,12 @@ public class DemoController {
         myCoach = coach;
     }
 @GetMapping("/dailyworkout")
-    public String gailyWorkout(){
+    public String dailyWorkout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/")
+    String homePage(){
+       return "this is the home page";
     }
 }
