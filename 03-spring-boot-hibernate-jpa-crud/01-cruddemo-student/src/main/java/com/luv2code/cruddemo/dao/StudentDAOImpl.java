de82d8d8c5407ea446d8e8ad0feaf1427e2cea8e
arjun-chagal <2021is_arjunchagalamarri_a@nie.ac.in>
2025-06-07 23:41:48 +0530

    coded a student DAO that communicates with the entity manager

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,32 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+//studentDAO is used for interacting with the entity manager.
+//
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+
+    //define a private field entityManager
+    private EntityManager entityManager;
+
+
+    //inject entityManager by constructor injection
+    @Autowired
+    StudentDAOImpl(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
+    //save a java object item using the entityManager
+    @Override
+    @Transactional
+    public void save(Student theStudent){
+        entityManager.persist(theStudent);
+
+    }
+}
