63e085b96b543ed484c756d6aaa5e9351ad37cef
arjun-chagal <2021is_arjunchagalamarri_a@nie.ac.in>
2025-07-18 20:38:49 +0530

    modified loading data; now list is created only once(when the bean is loaded)

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -2,6 +2,7 @@ package com.luv2code.demo.rest;
 
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,20 +13,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-
-    @GetMapping("/students")
-    public List<Student> returnStudent(){
-        //hardcoded adding students
-        //will directly connect to the DB later
-        List<Student> theStudents = new ArrayList<>();
-
+    private List<Student> theStudents;
+    //postconstruct and other lifecycle bean annotations
+    // are only valid when theyre in the bean body, and not in any methods in the bean body
+    @PostConstruct
+    public void loadStudents(){
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Arjun", "Chagal"));
         theStudents.add(new Student("Max", "Verstappen"));
         theStudents.add(new Student("Oscar", "Piastri"));
         theStudents.add(new Student("Ayrton", "Senna"));
+    }
 
+
+    @GetMapping("/students")
+    public List<Student> returnStudent(){
         return theStudents;
-
     }
 
 
